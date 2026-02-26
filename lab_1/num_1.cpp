c192b0b2ea0ccf8f793a3afab11e319f6a4f70c7
Shlepabot <114918818+Xcx9@users.noreply.github.com>
2026-02-26 12:48:47 +0300

    Create num_1.cpp

---
+++ lab_1/num_1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    puts("Gruppa: 8\n"
+         "Laboratornaya rabota №1\n"
+         "Sostav komandy:\n"
+         "Ivanov I.I.\n"
+         "Petrov P.P.");
+
+    return 0;
+}
